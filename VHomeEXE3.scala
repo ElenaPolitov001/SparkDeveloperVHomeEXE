9386a85246aae344e60eb59274c0d343a59a4e49
ElenaPolitov001 <93623352+ElenaPolitov001@users.noreply.github.com>
2025-04-02 18:12:42 +0300

    Create VHomeEXE3.scala
     on scala 3

---
+++ VHomeEXE3.scala
@@ -0,0 +1,8 @@
+// GraphXAppTest.scala
+import org.scalatest.flatspec.AnyFlatSpec
+
+class GraphXAppTest extends AnyFlatSpec {
+  "GraphXApp" should "initialize correctly" in {
+    // Your test logic here
+  }
+}
